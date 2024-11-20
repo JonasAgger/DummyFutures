2c9289b91512040d9a402aab2e64290240abbce7
Jonas Agger <jaj@mft-energy.com>
2024-11-20 21:16:53 +0100

    wat

---
+++ rubik/src/task.rs
@@ -0,0 +1,14 @@
+use std::future::Future;
+
+pub struct Task {}
+
+impl Future for Task {
+    type Output = ();
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        todo!()
+    }
+}
